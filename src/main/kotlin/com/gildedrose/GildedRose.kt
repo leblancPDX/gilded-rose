d8e3e020437c937e3689b86204207addde6af185
dleblanc <dleblanc@ebay.com>
2025-07-14 21:14:00 -0700

    Phase 2 refactors the project to use classes instead of functions.

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,41 +3,61 @@ package com.gildedrose
 class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            val item = items[i]
-            when {
-                item.name.startsWith("Aged Brie") -> updateAgedBrie(item)
-                item.name.startsWith("Backstage passes") -> updateBackStagePasses(item)
-                item.name.startsWith("Sulfuras") -> updateLegendaryItem()
-                else -> updateItem(item)
-            }
+        items.forEach { item ->
+            val itemType = typeOf(item)
+            itemType.update(item)
         }
     }
+}
 
-    private fun updateAgedBrie(item: Item) {
-        item.sellIn -= 1
-        val qualityIncrease = if (item.sellIn < 0) 2 else 1
-        item.quality = (item.quality + qualityIncrease).coerceAtMost(50)
+open class BasicItem {
+    fun update(item: Item) {
+        age(item)
+        degrade(item)
     }
 
-    private fun updateBackStagePasses(item: Item) {
-        item.quality = when {
-            item.sellIn <= 0 -> 0
-            item.sellIn <= 5 -> item.quality + 3
-            item.sellIn <= 10 -> item.quality + 2
-            else -> item.quality + 1
-        }.coerceAtMost(50)
+    protected open fun age(item: Item) {
         item.sellIn -= 1
     }
 
-    private fun updateItem(item: Item) {
-        item.sellIn -= 1
+    protected open fun degrade(item: Item) {
         item.quality = when {
-            item.quality <= 0 -> 0
             item.sellIn < 0 -> item.quality - 2
             else -> item.quality - 1
         }
+        item.quality = item.quality.coerceAtLeast(0)
     }
+}
 
-    private fun updateLegendaryItem() {}
+object AgedBrie : BasicItem() {
+    override fun degrade(item: Item) {
+        val qualityIncrease = if (item.sellIn < 0) 2 else 1
+        item.quality = (item.quality + qualityIncrease).coerceAtMost(50)
+    }
+}
+
+object BackstagePass : BasicItem() {
+    override fun degrade(item: Item) {
+        item.quality = when {
+            item.sellIn < 0 -> 0
+            item.sellIn < 5 -> item.quality + 3
+            item.sellIn < 10 -> item.quality + 2
+            else -> item.quality + 1
+        }
+        item.quality = item.quality.coerceAtMost(50)
+    }
+}
+
+object Legendary : BasicItem() {
+    override fun age(item: Item) {}
+    override fun degrade(item: Item) {}
+}
+
+fun typeOf(item: Item): BasicItem {
+    return when {
+        item.name.startsWith("Aged Brie") -> AgedBrie
+        item.name.startsWith("Backstage passes") -> BackstagePass
+        item.name.startsWith("Sulfuras") -> Legendary
+        else -> BasicItem()
+    }
 }
