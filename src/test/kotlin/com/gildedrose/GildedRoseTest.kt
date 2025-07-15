0d2a722262ede17e59c6134f8b3f5119a4864c0b
dleblanc <dleblanc@ebay.com>
2025-07-14 19:14:54 -0700

    Created simple test to see if behavior deviates from baseline.

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,15 +2,27 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
 
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = listOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+    fun `should match expected output for 100 days`() {
+        val expected = File("src/test/resources/expected_output.txt").readText()
+        val actual = captureOutputFromMain()
+        assertEquals(expected, actual)
+    }
+
+    private fun captureOutputFromMain(): String {
+        val originalOutStream = System.out
+        val outputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(outputStream))
+        main(arrayOf("100"))
+        System.setOut(originalOutStream)
+
+        return outputStream.toString()
     }
 
 }
