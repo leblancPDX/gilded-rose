85af5090fae18e3d56a0438121bcbc8b66a5949d
dleblanc <dleblanc@ebay.com>
2025-07-14 19:38:43 -0700

    Phase 1 refactoring removes the messy nested conditionals and replaces them with cleaner and more readable functions

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,54 +4,40 @@ class GildedRose(val items: List<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+            val item = items[i]
+            when {
+                item.name.startsWith("Aged Brie") -> updateAgedBrie(item)
+                item.name.startsWith("Backstage passes") -> updateBackStagePasses(item)
+                item.name.startsWith("Sulfuras") -> updateLegendaryItem()
+                else -> updateItem(item)
             }
         }
     }
 
+    private fun updateAgedBrie(item: Item) {
+        item.sellIn -= 1
+        val qualityIncrease = if (item.sellIn < 0) 2 else 1
+        item.quality = (item.quality + qualityIncrease).coerceAtMost(50)
+    }
+
+    private fun updateBackStagePasses(item: Item) {
+        item.quality = when {
+            item.sellIn <= 0 -> 0
+            item.sellIn <= 5 -> item.quality + 3
+            item.sellIn <= 10 -> item.quality + 2
+            else -> item.quality + 1
+        }.coerceAtMost(50)
+        item.sellIn -= 1
+    }
+
+    private fun updateItem(item: Item) {
+        item.sellIn -= 1
+        item.quality = when {
+            item.quality <= 0 -> 0
+            item.sellIn < 0 -> item.quality - 2
+            else -> item.quality - 1
+        }
+    }
+
+    private fun updateLegendaryItem() {}
 }
